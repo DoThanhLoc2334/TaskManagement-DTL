76e5d5c86eaf3048f3795d8a5ae45160265fe730
DoThanhLoc2334 <locdothanh233@gmail.com>
2026-02-24 14:59:40 +0700

    feat(Board): implement create and get board API (in -memory)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const boardRouter = require('./routes/board.routes');
 const app = express();
 app.get('/', (req, res) => {
     res.send('API is running...');
 })
 
+app.use(express.json());
+app.use('/boards', boardRouter);
 module.exports = app;
---
+++ src/controllers/board.controller.js
@@ -0,0 +1,34 @@
+let boards = [];
+let currentID = 1;
+const createBoard = (req, res) => {
+    const { title } = req.body
+    if (!title) {
+        return res.status(400).json({
+            success: false,
+            message: "Title is required"
+        })
+    }
+    const newBoard = {
+        id: currentID++,
+        title,
+        createBoard: new Date()
+    }
+
+    boards.push(newBoard);
+    res.status(201).json({
+        success: true,
+        data: newBoard
+    })
+}
+
+const getBoards = (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: boards
+    })
+}
+
+module.exports = {
+    createBoard,
+    getBoards
+}
