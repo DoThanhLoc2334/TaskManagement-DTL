5dc92cd8e20e943012d30cab71d2a936a00283f7
DoThanhLoc2334 <locdothanh233@gmail.com>
2026-02-25 20:50:36 +0700

    fix(list): fix boardId undefind in routes
    const router = express.Router({ mergeParams: true })

--- src/controllers/list.controller.js
+++ src/controllers/list.controller.js
@@ -1,43 +1,46 @@
 let lists = []
-let currentList = 1
+let currentListId = 1
 
 const createList = (req, res) => {
     const { boardId } = req.params
     const { title } = req.body
 
     if (!title) {
-        return res.status(401).json({
+        return res.status(400).json({
             success: false,
             message: "Title is required"
         })
     }
 
     const newList = {
-        id: currentId++,
+        id: currentListId++,
         title,
         boardId: Number(boardId),
         position: lists.length + 1,
-        createAt: new Date()
+        createdAt: new Date()
     }
+
     lists.push(newList)
 
     res.status(201).json({
         success: true,
         data: newList
     })
-
-    const getListsByBoard = (req, res) => {
-        const { boardId } = req.params
-        const boardLists = lists.filter(
-            list => list.boardId === Number(boardId)
-        )
-
-        res.status(200).json({
-            success: true,
-            data: boardLists
-        })
-    }
 }
+
+const getListsByBoard = (req, res) => {
+    const { boardId } = req.params
+
+    const boardLists = lists.filter(
+        list => list.boardId === Number(boardId)
+    )
+
+    res.status(200).json({
+        success: true,
+        data: boardLists
+    })
+}
+
 module.exports = {
     createList,
     getListsByBoard
--- src/routes/list.routes.js
+++ src/routes/list.routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const router = express.Router({ mergeParams: true })
+
 const listController = require('../controllers/list.controller')
-router.post('/', listController, createList)
-router.get('/', listController, getListsByBoard)
+router.post('/', listController.createList)
+router.get('/', listController.getListsByBoard)
 module.exports = router
